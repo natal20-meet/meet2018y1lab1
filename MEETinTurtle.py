75e7d4aa8bee7dd9cbd39e9e580839827639b0eb
natal20-meet <natal20@meet.mit.edu>
2018-07-29 11:20:13 -0400

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,43 @@
+import turtle
+
+# Everything that comes aftr the # is a
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+turtle.penup() #Pick up the pen so it doesn't
+               #draw
+turtle.goto(-200,-100) #Move the turle to the
+                        #position  (-200, -100)
+                        #on the screen
+turtle.pendown() #Put the pen down to start
+                #drawing
+#Draw the M:
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(-50,-100+200)
+turtle.goto(0,-100+200)
+turtle.penup()
+turtle.goto(-50,-100+100)
+turtle.pendown()
+turtle.goto(0,-100+100)
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(0,-100)
+tutle.penup()
+turtle.goto(0+50,-100)
+
+
+
+            
+
+# ...and end it before the next line.
+turtle.mainloop()
+
+
